793e1a20fbca753ff2dc7cb7d3faf54ac5642704
diwanesahil <dada.diwane001@gmail.com>
2025-11-22 20:02:16 +0530

    lkji

--- storage.tf
+++ storage.tf
@@ -25,17 +25,21 @@ resource "azurerm_private_endpoint" "StorageAccount_Private_Endpoint" {
     subresource_names              = ["blob"]
         
   }
-
-  private_dns_zone_group {
-    name                 = "StorageAccountDNSZoneGroup"
-    private_dns_zone_ids = [azurerm_private_dns_zone.StorageAccount_Private_DNS.id]
+ 
   }
 
-  }
 # Create Private DNS Zone for Storage Account
 resource "azurerm_private_dns_zone" "StorageAccount_Private_DNS" {
   name                = "privatelink.blob.core.windows.net"
   resource_group_name = azurerm_resource_group.Network_RG.name
+}
+
+# Create Private DNS Zone Group
+resource "azurerm_private_dns_zone_group" "StorageAccount_DNS_Zone_Group" {
+  name                 = "StorageAccountDNSZoneGroup"
+    private_dns_zone_ids = [
+    azurerm_private_dns_zone.StorageAccount_Private_DNS.id,
+  ]
 }
 
 # Create Private DNS Zone Virtual Network Link
@@ -43,6 +47,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "StorageAccount_DNS_VNe
   name                  = "StorageAccountDNSVNetLink"
   resource_group_name   = azurerm_resource_group.Network_RG.name
   private_dns_zone_name = azurerm_private_dns_zone.StorageAccount_Private_DNS.name
-  virtual_network_id    = azurerm_virtual_network.Hub_Vnet.id
+  virtual_network_id    = azurerm_virtual_network.Spoke_Vnet.id
   registration_enabled  = false
 }
