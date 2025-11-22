8522fbaf2ea65cc811b012e03987eb99cc356c3c
diwanesahil <dada.diwane001@gmail.com>
2025-11-22 17:28:49 +0530

    kjfff

--- storage.tf
+++ storage.tf
@@ -14,11 +14,14 @@ resource "azurerm_private_endpoint" "StorageAccount_Private_Endpoint" {
   location            = azurerm_resource_group.Network_RG.location
   resource_group_name = azurerm_resource_group.Network_RG.name
   subnet_id          = azurerm_subnet.Web_Subnet.id
+  
 
   private_service_connection {
     name                           = "StorageAccountConnection"
     private_connection_resource_id = azurerm_storage_account.StorageAccount.id
     is_manual_connection           = false
+    subresource_names              = ["blob"]
+    
     
   }
 }
